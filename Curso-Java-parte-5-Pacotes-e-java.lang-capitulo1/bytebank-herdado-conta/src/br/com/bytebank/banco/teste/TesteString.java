60415eba236fbab6c663cbf1d1488284b8f5333c
Lucas da Silva Santos <lucas.parkl@outlook.com>
2022-03-17 18:32:47 -0300

    Java e java.lang: finalização do modulo 4: - estudo de como a lib java lang é implementada implicitamente dentro do código por ser essencial para o funcionalmento de um programa java

---
+++ Curso-Java-parte-5-Pacotes-e-java.lang-capitulo1/bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteString.java
@@ -0,0 +1,16 @@
+package br.com.bytebank.banco.teste;
+
+public class TesteString {
+
+	public static void main(String[] args) {
+		int a = 3;
+		String nome = "alura";
+		//String antigoNome = new String("alura"); pode ser inicializado assim
+		nome = nome.replace("a", "A");
+		
+		System.out.println(nome);
+		
+
+	}
+
+}
